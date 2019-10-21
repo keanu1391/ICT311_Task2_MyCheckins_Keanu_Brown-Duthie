c13d57543a0c831579ba04687b781198572f754d
keanu brown-duthie <KeanuBrown-Duthie@hotmail.com>
2019-10-22 01:27:24 +1000

    Checkin Java Class Created

---
+++ MyCheckins/app/src/main/java/com/keanu1094859/mycheckins/Checkin.java
@@ -0,0 +1,82 @@
+package com.keanu1094859.mycheckins;
+
+import android.net.Uri;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class Checkin {
+    private UUID mId;
+    private String mTitle;
+    private String mPlace;
+    private String mDetails;
+    private Date mDate;
+    private Uri mImage;
+    private Float mLongitude;
+    private Float mLatitude;
+
+    public Checkin() {
+        mId = UUID.randomUUID();
+        mDate = new Date();
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setmTitle(String title) {
+        mTitle = title;
+    }
+
+    public String getPlace() {
+        return mPlace;
+    }
+
+    public void setPlace(String place) {
+        mPlace = place;
+    }
+
+    public String getDetails() {
+        return mDetails;
+    }
+
+    public void setDetails(String details) {
+        mDetails = details;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public Uri getImage() {
+        return mImage;
+    }
+
+    public void setImage(Uri imageUri) {
+        mImage = imageUri;
+    }
+
+    public Float getLongitude() {
+        return mLongitude;
+    }
+
+    public void setLongitude(Float longitude) {
+        mLongitude = longitude;
+    }
+
+    public Float getLatitude() {
+        return mLatitude;
+    }
+
+    public void setLatitude(Float latitude) {
+        mLatitude = latitude;
+    }
+}
